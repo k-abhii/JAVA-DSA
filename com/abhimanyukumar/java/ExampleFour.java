2b1ddd5cc6c98f075732d38e18b5b40dcb4da6f7
Abhimanyu_Kumar <kabhimanyu656@gmail.com>
2022-03-07 18:42:08 +0530

    Input updated

--- com/abhimanyukumar/java/ExampleFour.java
+++ com/abhimanyukumar/java/ExampleFour.java
@@ -4,9 +4,10 @@ public class ExampleFour {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         System.out.println("Enter Your First Name");
-        System.out.println(" First Name : "+scan.nextLine());
+        String firstName = scan.nextLine();
         System.out.println(" Entar Your Last Name");
-        System.out.println("Last Name : "+scan.nextLine());
+        String lastName = scan.nextLine();
+        System.out.println(" First Name : "+firstName+" Last Name : "+lastName);
     }
     
 }
