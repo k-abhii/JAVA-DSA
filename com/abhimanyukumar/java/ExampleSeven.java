69c152303717555982821548747d0e75ad591564
Abhimanyu_Kumar <kabhimanyu656@gmail.com>
2022-03-07 21:01:46 +0530

    Decision making

---
+++ com/abhimanyukumar/java/ExampleSeven.java
@@ -0,0 +1,15 @@
+package com.abhimanyukumar.java;
+/* decision Making*/
+
+public class ExampleSeven {
+    public static void main(String[] args) {
+        int x=80;
+        int y=50;
+        if(x>y){
+            System.out.println(x+ " greater than "+y);
+        }else{
+            System.out.println(y+" greater than "+x);
+        }
+    }
+    
+}
