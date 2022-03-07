7d51df6def52174db04961df9c3df0578e2f4760
Abhimanyu_Kumar <kabhimanyu656@gmail.com>
2022-03-07 19:49:23 +0530

    Type Casting

---
+++ com/abhimanyukumar/java/ExampleSix.java
@@ -0,0 +1,14 @@
+package com.abhimanyukumar.java;
+/*Type Casting*/
+public class ExampleSix {
+    public static void main(String[] args) {
+        double y=100.00D;
+        int x = (int)y;
+        float d= 200.50F;
+        int c = (int)d;
+        System.out.println(" X : "+x);
+        System.out.println(" C : "+c);
+        // N.B it is not possible to convert a string/character/ boolean to int
+    }
+    
+}
