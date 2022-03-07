7468bc72e2295b2d0065cae1623c626f8c7ba159
Abhimanyu_Kumar <kabhimanyu656@gmail.com>
2022-03-07 18:12:43 +0530

    primitive data types

---
+++ com/abhimanyukumar/java/ExampleThree.java
@@ -0,0 +1,14 @@
+package com.abhimanyukumar.java;
+
+public class ExampleThree {
+    public static void main(String[] args) {
+        int x = 10;
+        float y = 200.00F;
+        char z = 'A';
+        double d = 100.00D;
+        boolean bool = true;
+        String str = " Abhimanyu ";
+        System.out.println(" X " + x + " Y " + y + " Z : "+z + " D " + d + " BOOLEAN " + bool + " String " + str);
+    }
+
+}
