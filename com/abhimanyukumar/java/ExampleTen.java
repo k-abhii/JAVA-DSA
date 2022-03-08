4b78912b7ab8cfca48ef33c60d50902150ee09cd
Abhimanyu_Kumar <kabhimanyu656@gmail.com>
2022-03-08 12:08:51 +0530

    for loop and aray

---
+++ com/abhimanyukumar/java/ExampleTen.java
@@ -0,0 +1,13 @@
+package com.abhimanyukumar.java;
+
+public class ExampleTen {
+    public static void main(String[] args) {
+        int [] x = { 10,20,30,40,50,25};
+        int [] y = new int[6];
+        for(int i=0;i<x.length;i++){
+            y[i]=x[i];
+            System.out.println(" y [ "+i+" ] = "+y[i]);
+        }
+    }
+    
+}
