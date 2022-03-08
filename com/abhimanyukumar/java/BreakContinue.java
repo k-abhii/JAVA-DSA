aa0599b768607949328529943393cf046385e9d0
Abhimanyu_Kumar <kabhimanyu656@gmail.com>
2022-03-08 11:36:30 +0530

    Continue

--- com/abhimanyukumar/java/BreakContinue.java
+++ com/abhimanyukumar/java/BreakContinue.java
@@ -3,10 +3,14 @@ package com.abhimanyukumar.java;
 public class BreakContinue {
     public static void main(String[] args) {
         for(int i=1;i<=10;i++){
-            System.out.println(" i = "+i);
-            if(i==5){
+            //System.out.println(" i = "+i);
+            /*if(i==5){
                 break;//Loop gets terminated when i becomes 5
+            }*/
+            if(i==5){
+                continue;// Going for begginning of loop At that iteration skipped
             }
+            System.out.println(" i = "+i);
         }
     }
     
